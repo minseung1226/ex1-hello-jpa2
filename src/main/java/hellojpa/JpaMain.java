9366210141d1eefeb08578360d9191bf3e5008a4
minseung1226 <minseung1226@naver.com>
2023-02-27 22:32:57 +0900

    영속성 관리(내부 동작 방식) - 영속성 컨텍스트, 플러시,준 영속 상태

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -12,22 +12,22 @@ public class JpaMain {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
-        EntityTransaction tx=null;
+        EntityTransaction tx=em.getTransaction();
+        tx.begin();
         try{
-            tx = em.getTransaction();
-            tx.begin();
 
             //저장
             Member member = new Member();
             member.setName("member1");
             em.persist(member);
 
-            //수정
+
+   /*         //수정
             Member findMember = em.find(Member.class, member.getId());
 
             //삭제
             findMember.setName("member2");
-            em.remove(findMember);
+            em.remove(findMember);*/
 
             tx.commit();
         }catch (Exception e){
@@ -55,6 +55,86 @@ public class JpaMain {
                     .setFirstResult(5)
                     .setMaxResults(5)
                     .getResultList();
+
+            tx.commit();
+        }catch (Exception e){
+            tx.rollback();
+        }finally {
+            em.close();
+        }
+        emf.close();
+    }
+
+    public static void test2(){
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx=null;
+        try{
+            tx = em.getTransaction();
+            tx.begin();
+
+            Member member = new Member();
+            member.setName("HelloJPA");
+
+            System.out.println("===Before===");
+            em.persist(member);
+            System.out.println("===after===");
+
+            Member findMember = em.find(Member.class, member.getId());
+
+
+
+            tx.commit();
+        }catch (Exception e){
+            tx.rollback();
+        }finally {
+            em.close();
+        }
+        emf.close();
+    }
+
+    public static void test3(){
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx=null;
+        try{
+            tx = em.getTransaction();
+            tx.begin();
+
+            Member findMember1 = em.find(Member.class, 1L);
+            Member findMember2 = em.find(Member.class, 1L);
+
+            System.out.println("findMember1==findMember2 ? "+(findMember1==findMember2));
+
+            tx.commit();
+        }catch (Exception e){
+            tx.rollback();
+        }finally {
+            em.close();
+        }
+        emf.close();
+    }
+
+
+    public static void test4(){
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx=null;
+        try{
+            tx = em.getTransaction();
+            tx.begin();
+
+            Member member1 = new Member();
+            member1.setName("memberA");
+
+            Member member2 = new Member();
+            member2.setName("memberB");
+
+            em.persist(member1);
+            em.persist(member2);
+
+            Member findMember = em.find(Member.class, member1.getId());
+            findMember.setName("memberChange");
 
             tx.commit();
         }catch (Exception e){
